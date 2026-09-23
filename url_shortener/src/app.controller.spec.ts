6b379473bd695b204a9de9f51ae866fd47ac153f
Fabricio Liquesley <fabricio.learning@gmail.com>
2026-09-23 11:30:31 -0300

    url_shortener: I finished setting up the project with NestJS and FastifyAdapter

--- url_shortener/src/app.controller.spec.ts
+++ url_shortener/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "It\'s working"', () => {
+      expect(appController.getHealth()).toBe("It's working");
     });
   });
 });
